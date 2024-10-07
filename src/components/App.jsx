320dcb7fc5e3114721ca779f51397f57f9325966
Lucy Moore <lucy.moore@devacademy.co.nz>
2024-10-08 10:56:31 +1300

    react set up and different components display correctly

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import Textbox from './textbox'
+import Experience from './Experience'
+
+function App() {
+  return (
+    <>
+      <div>
+        here is the content
+        <Textbox></Textbox>
+        <Experience></Experience>
+      </div>
+    </>
+  )
+}
+
+export default App
